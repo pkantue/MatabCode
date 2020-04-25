e1b78fb66318971e29c78260406e09a9f575fbda
Paulin Kantue <pkantue@gmail.com>
2020-04-25 21:13:08 +0100

    started recording covid19 data (South Africa) for modeling and analysis algorithm development.

---
+++ covid2019analyzer.m
@@ -0,0 +1,41 @@
+% This script is to retrieve, analyze and plot data from around the world
+% for public awareness
+% This script also has prediction logic just for analysis purposes and not
+% to be used in public social platforms. The Author accepts no
+% responsibility in the usage or distribution of this script.
+%
+% This focus is on South Africa 
+% 
+% Sources:
+% https://www.worldometers.info/coronavirus/
+% 
+ 
+% Author: Paulin Kantue
+% Date: April 2020
+
+% clearvars;
+
+%% retrieve data (automatic)
+% 1 - get Gauteng data from 
+% data = retrieveData(1);
+
+%% retrieve data (manual)
+data{1,1,1}.level = 'Province';
+n = size(data,1);
+data{n,1,1}.RowTag = {'Unidentified',...
+    'Northern Cape',...
+    'North West',...
+    'Mpumalanga',...
+    'Limpopo',...
+    'Eastern Cape',...
+    'Free State',...
+    'KwaZulu- Natal',...
+    'Western Cape',...
+    'Gauteng'};
+
+data{n,1,1}.ColumnTag = {'Accumulated Cases',...
+    'Daliy Death'};
+data{n,1,1}.date = '25-Apr-2020';
+
+%% save data 
+save('Covid2019_Archived_Data/covid19SA.mat','data');
