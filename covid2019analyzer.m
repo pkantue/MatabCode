2a00ba1b28e122a48918821b087ac11c6d7b4b7c
Paulin Kantue <pkantue@gmail.com>
2020-04-25 22:22:12 +0100

    Decided to start the data from the first lockdown (27th March 2020. Also included manual input of data with descriptive tags.

--- covid2019analyzer.m
+++ covid2019analyzer.m
@@ -20,8 +20,15 @@
 % data = retrieveData(1);
 
 %% retrieve data (manual)
-data{1,1,1}.level = 'Province';
+load('Covid2019_Archived_Data/covid19SA.mat');
 n = size(data,1);
+
+% update
+% n = n + 1;
+
+% data structure
+
+data{n,1,1}.level = 'Province';
 data{n,1,1}.RowTag = {'Unidentified',...
     'Northern Cape',...
     'North West',...
@@ -35,9 +42,26 @@ data{n,1,1}.RowTag = {'Unidentified',...
 
 data{n,1,1}.ColumnTag = {'Accumulated Cases',...
     'Daliy Death'};
-data{n,1,1}.date = '25-Apr-2020';
+data{n,1,1}.date = '27-Mar-2020';
 data{n,1,1}.testTag = {'Accumulated','Daily'};
 data{n,1,1}.testing = [161004 8614];
+
+% user input 
+data{n,1,1}.date = input('Date of Dataset [dd-mmm-yyyy]: ','s');
+
+% collect table data
+data{n,1,1}.table = zeros(length(data{n,1,1}.RowTag),...
+    length(data{n,1,1}.ColumnTag));
+
+for i = 1:length(data{n,1,1}.RowTag)
+    for j = 1:length(data{n,1,1}.ColumnTag)
+    data{n,1,1}.table(i,j) = input([data{n,1,1}.ColumnTag{1,j} ' - '...
+        data{n,1,1}.RowTag{1,i} ': ']);
+    end
+end
+
+data{n,1,1}.testing(1) = input('Dataset of testing - Accumulated]: ');
+data{n,1,1}.testing(2) = input('Dataset of testing - Daily: ');
 
 %% save data 
 save('Covid2019_Archived_Data/covid19SA.mat','data');
