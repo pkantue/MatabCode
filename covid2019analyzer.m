44be5ba6184c90fed4b0728b0ecab1cdff12c200
Paulin Kantue <pkantue@gmail.com>
2020-04-26 15:12:20 +0100

    captured data for up to the 20 April 2020. Certain tables still needs to be sorted per provinces.

--- covid2019analyzer.m
+++ covid2019analyzer.m
@@ -8,12 +8,31 @@
 % 
 % Sources:
 % https://www.worldometers.info/coronavirus/
+% https://www.covid19sa.org/provincial-breakdown/
+% https://www.nicd.ac.za/
+%
+%
+% Death statistics:
+% https://www.nicd.ac.za/covid-19-update-31/  ---> Up to 8-April-2020
+% https://www.nicd.ac.za/covid-19-update-33/  ---> Up to 15-April-2020
+% https://mobile.twitter.com/DrZweliMkhize/status/1250854262910976000  --->
+% Up to 16 April
 % 
- 
+%
+% Recoveries
+% https://www.nicd.ac.za/covid-19-update-32/ ---> Up to 10-April-2020
+% https://mobile.twitter.com/DrZweliMkhize/status/1250854262910976000  --->
+% Up to 16 April
+% https://www.nicd.ac.za/covid-19-update-36/ ---> Up to 20-April-2020
+%
+% Lab Capacity Report
+% https://www.nicd.ac.za/covid-19-update-36/ --> 20-April-2020
+%
+%
 % Author: Paulin Kantue
 % Date: April 2020
 
-% clearvars;
+clearvars;
 
 %% retrieve data (automatic)
 % 1 - get Gauteng data from 
@@ -24,7 +43,7 @@ load('Covid2019_Archived_Data/covid19SA.mat');
 n = size(data,1);
 
 % update
-% n = n + 1;
+n = n + 1;
 
 % data structure
 
@@ -41,10 +60,10 @@ data{n,1,1}.RowTag = {'Unidentified',...
     'Gauteng'};
 
 data{n,1,1}.ColumnTag = {'Accumulated Cases',...
-    'Daliy Death'};
-data{n,1,1}.date = '27-Mar-2020';
+    'Daily Death','Accumulated Recoveries'};
+data{n,1,1}.date = '';
 data{n,1,1}.testTag = {'Accumulated','Daily'};
-data{n,1,1}.testing = [161004 8614];
+data{n,1,1}.testing = [0 0];
 
 % user input 
 data{n,1,1}.date = input('Date of Dataset [dd-mmm-yyyy]: ','s');
@@ -53,8 +72,8 @@ data{n,1,1}.date = input('Date of Dataset [dd-mmm-yyyy]: ','s');
 data{n,1,1}.table = zeros(length(data{n,1,1}.RowTag),...
     length(data{n,1,1}.ColumnTag));
 
-for i = 1:length(data{n,1,1}.RowTag)
-    for j = 1:length(data{n,1,1}.ColumnTag)
+for j = 1:length(data{n,1,1}.ColumnTag)
+    for i = 1:length(data{n,1,1}.RowTag)
     data{n,1,1}.table(i,j) = input([data{n,1,1}.ColumnTag{1,j} ' - '...
         data{n,1,1}.RowTag{1,i} ': ']);
     end
